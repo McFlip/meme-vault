c70e38a2a55cffa3b448d0ab2854a7b6e892dc72
McFlip <gradydenton@yahoo.com>
2023-04-04 01:33:09 -0400

    getMemesByTags

--- src/__tests__/db-getMemesByTags.ts
+++ src/__tests__/db-getMemesByTags.ts
@@ -1,9 +1,45 @@
 import { dbGetMemesByTags } from '~/db/queries'
 
 describe('db query to get all memes matching every listed tag', () => {
-  it('filters memes on tags', async () => {
-    const expected = [{meme_url: 'https://pbs.twimg.com/media/Et1kXL0XMAUj5Iw?format=jpg&name=small'}]
-    const actual = await dbGetMemesByTags(['fubar'])
+  it('finds the lone bohica tag', async () => {
+    const expected = ['https://i.ytimg.com/vi/ris-1JmOg-U/maxresdefault.jpg']
+    const actual = await dbGetMemesByTags(['bohica'])
     expect(actual).toEqual(expected)
+  }),
+  it('finds the 3 fubar tags', async () => {
+    const expectedLength = 3
+    const expectedTag1 = 'https://pbs.twimg.com/media/Et1kXL0XMAUj5Iw?format=jpg&name=small'
+    const expectedTag2 = 'https://preview.redd.it/tnl3actbwk651.png?auto=webp&s=a1bcfc72a69e355a23f860f2e20b9ed85033665c'
+    const expectedTag3 = 'https://forums.frontier.co.uk/attachments/doom_meow-jpg.300893/'
+    const actual = await dbGetMemesByTags(['fubar'])
+    expect(actual.length).toEqual(expectedLength)
+    expect(actual).toContainEqual(expectedTag1)
+    expect(actual).toContainEqual(expectedTag2)
+    expect(actual).toContainEqual(expectedTag3)
+  }),
+  it('finds the 2 snafu tags', async () => {
+    const expectedLength = 2
+    const expectedTag2 = 'https://preview.redd.it/tnl3actbwk651.png?auto=webp&s=a1bcfc72a69e355a23f860f2e20b9ed85033665c'
+    const expectedTag3 = 'https://forums.frontier.co.uk/attachments/doom_meow-jpg.300893/'
+    const actual = await dbGetMemesByTags(['snafu'])
+    expect(actual.length).toEqual(expectedLength)
+    expect(actual).toContainEqual(expectedTag2)
+    expect(actual).toContainEqual(expectedTag3)
+  }),
+  it('finds the 2 memes that have both fubar and snafu tags', async () => {
+    const expectedLength = 2
+    const expectedTag2 = 'https://preview.redd.it/tnl3actbwk651.png?auto=webp&s=a1bcfc72a69e355a23f860f2e20b9ed85033665c'
+    const expectedTag3 = 'https://forums.frontier.co.uk/attachments/doom_meow-jpg.300893/'
+    const actual = await dbGetMemesByTags(['fubar','snafu'])
+    expect(actual.length).toEqual(expectedLength)
+    expect(actual).toContainEqual(expectedTag2)
+    expect(actual).toContainEqual(expectedTag3)
+  }),
+  it('finds the 1 meme that has the fubar, wtf, and snafu tags', async () => {
+    const expectedLength = 1
+    const expectedTag3 = 'https://forums.frontier.co.uk/attachments/doom_meow-jpg.300893/'
+    const actual = await dbGetMemesByTags(['fubar','snafu','wtf'])
+    expect(actual.length).toEqual(expectedLength)
+    expect(actual).toContainEqual(expectedTag3)
   })
 })
--- src/db/queries.ts
+++ src/db/queries.ts
@@ -5,8 +5,10 @@ import { memeTags } from "./schema"
 
 export const dbSearchTags = async (qstr: string) => await db.select({tag: memeTags.tag}).from(memeTags).where(like(memeTags.tag, `%${qstr}%`))
 export const dbGetMemesByTags = async (tags:string[]) => {
-  const orExpression = tags.map(tag => eq(memeTags.tag, tag))
-  return await db.select({meme_url: memeTags.memeUrl})
-    .from(memeTags)
-    .where(and(...orExpression))
+  // run 1 sub-query per tag
+  const subQueriesPromise = tags.map(async tag => await db.select({meme_url: memeTags.memeUrl}).from(memeTags).where(eq(memeTags.tag, tag)))
+  const subQueries = await Promise.all(subQueriesPromise)
+  // inner join all of the sub-queries
+  // flatten all the objects to make includes() func easier
+  return subQueries.map(table => table.flatMap(row => row.meme_url)).reduce((prev, curr) => curr.filter(row => prev.includes(row)))
 }

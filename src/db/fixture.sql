88f6987426deaa7831e506b284d64dd81bc9ea00
McFlip <gradydenton@yahoo.com>
2023-04-04 23:29:56 -0400

    backup testing db as fixture.sql

---
+++ src/db/fixture.sql
@@ -0,0 +1,36 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.2 (Debian 15.2-1.pgdg110+1)
+-- Dumped by pg_dump version 15.2 (Debian 15.2-1.pgdg110+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: meme_tags; Type: TABLE DATA; Schema: public; Owner: dev
+--
+
+COPY public.meme_tags (meme_url, tag) FROM stdin;
+https://pbs.twimg.com/media/Et1kXL0XMAUj5Iw?format=jpg&name=small       fubar
+https://i.ytimg.com/vi/ris-1JmOg-U/maxresdefault.jpg    bohica
+https://preview.redd.it/tnl3actbwk651.png?auto=webp&s=a1bcfc72a69e355a23f860f2e20b9ed85033665c  fubar
+https://preview.redd.it/tnl3actbwk651.png?auto=webp&s=a1bcfc72a69e355a23f860f2e20b9ed85033665c  snafu
+https://forums.frontier.co.uk/attachments/doom_meow-jpg.300893/ fubar
+https://forums.frontier.co.uk/attachments/doom_meow-jpg.300893/ snafu
+https://forums.frontier.co.uk/attachments/doom_meow-jpg.300893/ wtf
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--

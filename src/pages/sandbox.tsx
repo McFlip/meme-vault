09eb425e8827048b0e0195f12b881c368af73700
McFlip <gradydenton@yahoo.com>
2023-04-05 23:46:56 -0400

    stop searing my eyes

--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -11,17 +11,19 @@ const Test: NextPage = () => {
   const { data } = api.tags.getMemesByTags.useQuery({ tags: selectedTags }, { enabled: selectedTags.length > 0 })
   return (
     <>
-      <MainMenu />
-      <h1>FUBAR</h1>
-      <div>
-        <SearchBar selectedTags={selectedTags} selectTag={selectTag} />
-      </div>
-      <div>
-        <TagList tags={selectedTags} handleClick={tag => selectTag(selectedTags.filter(t => t != tag))} />
-      </div>
-      <div>
-        {!!data && <Carousel memes={data.memes} />}
-      </div>
+      <main className="flex flex-col flex-wrap flex-none items-start content-center justify-start min-h-screen bg-gradient-to-b from-slate-600 to-red-900">
+        <MainMenu />
+        <h1>FUBAR</h1>
+        <div>
+          <SearchBar selectedTags={selectedTags} selectTag={selectTag} />
+        </div>
+        <div>
+          <TagList tags={selectedTags} handleClick={tag => selectTag(selectedTags.filter(t => t != tag))} />
+        </div>
+        <div>
+          {!!data && <Carousel memes={data.memes} />}
+        </div>
+      </main>
     </>
   )
 }

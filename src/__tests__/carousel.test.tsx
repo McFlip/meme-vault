885638bbc40a66f5fa8e4943116f1ef649af3f28
McFlip <gradydenton@yahoo.com>
2023-04-05 21:50:29 -0400

    change matcher in test for Nextjs Image

--- src/__tests__/carousel.test.tsx
+++ src/__tests__/carousel.test.tsx
@@ -5,24 +5,24 @@ import { Carousel } from '~/components/carousel'
 
 describe('Meme Carousel', () => {
   it('rotates through memes, stopping at the beginning and end', async () => {
-    const memes = ['1.jpg', '2.jpg', '3.jpg']
+    const memes = ['/doom_eternal.jpp', '/doom_lore.jpg', '/doom_meow.jpg']
     const user = userEvent.setup()
     render(<Carousel memes={memes} />)
     const prevBtn = screen.getByLabelText('Previous Slide')
     const nextBtn = screen.getByLabelText('Next Slide')
     const shownImg = screen.getByRole('img')
-    expect(shownImg.getAttribute('src')).toEqual(memes[0])
+    expect(shownImg.getAttribute('src')).toMatch(/doom_eternal/)
     await user.click(nextBtn)
-    expect(shownImg.getAttribute('src')).toEqual(memes[1])
+    expect(shownImg.getAttribute('src')).toMatch(/doom_lore/)
     await user.click(nextBtn)
-    expect(shownImg.getAttribute('src')).toEqual(memes[2])
+    expect(shownImg.getAttribute('src')).toMatch(/doom_meow/)
     await user.click(nextBtn)
-    expect(shownImg.getAttribute('src')).toEqual(memes[2])
+    expect(shownImg.getAttribute('src')).toMatch(/doom_meow/)
     await user.click(prevBtn)
-    expect(shownImg.getAttribute('src')).toEqual(memes[1])
+    expect(shownImg.getAttribute('src')).toMatch(/doom_lore/)
     await user.click(prevBtn)
-    expect(shownImg.getAttribute('src')).toEqual(memes[0])
+    expect(shownImg.getAttribute('src')).toMatch(/doom_eternal/)
     await user.click(prevBtn)
-    expect(shownImg.getAttribute('src')).toEqual(memes[0])
+    expect(shownImg.getAttribute('src')).toMatch(/doom_eternal/)
   })
 })

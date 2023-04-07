79b2fd98248e92088ee2160b12fc5b45536e3276
McFlip <gradydenton@yahoo.com>
2023-04-07 15:58:31 -0400

    router test concurrency

--- src/__tests__/tags-router.test.ts
+++ src/__tests__/tags-router.test.ts
@@ -5,7 +5,7 @@ import { inferProcedureInput } from "@trpc/server"
 describe("Tags Router", () => {
   const ctx = createInnerTRPCContext({})
   const caller = appRouter.createCaller(ctx)
-  it("searches tags by query string", async () => {
+  it.concurrent("searches tags by query string", async () => {
     const input: inferProcedureInput<AppRouter["tags"]["getSearchResults"]> = {
       qstr: "fub",
     }
@@ -14,7 +14,7 @@ describe("Tags Router", () => {
     expect(actual.tags[0]).toBe(expected)
     expect(actual.tags).toHaveLength(1)
   })
-  it("gets all memes matching tag filters", async () => {
+  it.concurrent("gets all memes matching tag filters", async () => {
     const input: inferProcedureInput<AppRouter["tags"]["getMemesByTags"]> = {
       tags: ["fubar"],
     }
@@ -28,7 +28,7 @@ describe("Tags Router", () => {
     const actual = await caller.tags.getMemesByTags(input)
     expect(actual).toEqual(expected)
   })
-  it("gets empty result due to AND condition", async () => {
+  it.concurrent("gets empty result due to AND condition", async () => {
     const input: inferProcedureInput<AppRouter["tags"]["getMemesByTags"]> = {
       tags: ["fubar", "bohica"],
     }
@@ -38,26 +38,32 @@ describe("Tags Router", () => {
     const actual = await caller.tags.getMemesByTags(input)
     expect(actual).toEqual(expected)
   })
-  it("finds the 2 memes that have both fubar and snafu tags", async () => {
-    const input: inferProcedureInput<AppRouter["tags"]["getMemesByTags"]> = {
-      tags: ["fubar", "snafu"],
+  it.concurrent(
+    "finds the 2 memes that have both fubar and snafu tags",
+    async () => {
+      const input: inferProcedureInput<AppRouter["tags"]["getMemesByTags"]> = {
+        tags: ["fubar", "snafu"],
+      }
+      const expectedLength = 2
+      const expectedUnicorn = "/img/doom_unicorn.jpg"
+      const expectedMeow = "/img/doom_meow.jpg"
+      const { memes: actual } = await caller.tags.getMemesByTags(input)
+      expect(actual.length).toEqual(expectedLength)
+      expect(actual).toContainEqual(expectedUnicorn)
+      expect(actual).toContainEqual(expectedMeow)
     }
-    const expectedLength = 2
-    const expectedUnicorn = "/img/doom_unicorn.jpg"
-    const expectedMeow = "/img/doom_meow.jpg"
-    const { memes: actual } = await caller.tags.getMemesByTags(input)
-    expect(actual.length).toEqual(expectedLength)
-    expect(actual).toContainEqual(expectedUnicorn)
-    expect(actual).toContainEqual(expectedMeow)
-  })
-  it("finds the 1 meme that has the fubar, wtf, and snafu tags", async () => {
-    const input: inferProcedureInput<AppRouter["tags"]["getMemesByTags"]> = {
-      tags: ["fubar", "wtf", "snafu"],
+  )
+  it.concurrent(
+    "finds the 1 meme that has the fubar, wtf, and snafu tags",
+    async () => {
+      const input: inferProcedureInput<AppRouter["tags"]["getMemesByTags"]> = {
+        tags: ["fubar", "wtf", "snafu"],
+      }
+      const expectedLength = 1
+      const expectedMeow = "/img/doom_meow.jpg"
+      const { memes: actual } = await caller.tags.getMemesByTags(input)
+      expect(actual.length).toEqual(expectedLength)
+      expect(actual).toContainEqual(expectedMeow)
     }
-    const expectedLength = 1
-    const expectedMeow = "/img/doom_meow.jpg"
-    const { memes: actual } = await caller.tags.getMemesByTags(input)
-    expect(actual.length).toEqual(expectedLength)
-    expect(actual).toContainEqual(expectedMeow)
-  })
+  )
 })

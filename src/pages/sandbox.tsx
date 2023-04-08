81cf38338a62ff732119109aafd50c8a793061bb
McFlip <gradydenton@yahoo.com>
2023-04-08 15:10:24 -0400

    sandbox

--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -8,7 +8,8 @@ import { Carousel } from "~/components/carousel";
 
 const Test: NextPage = () => {
   const [selectedTags, selectTag] = useState<string[]>([])
-  const { data } = api.tags.getMemesByTags.useQuery({ tags: selectedTags }, { enabled: selectedTags.length > 0 })
+  const { data: memeResults } = api.tags.getMemesByTags.useQuery({ tags: selectedTags }, { enabled: selectedTags.length > 0 })
+  const { data: availableTags } = api.tags.getAvailableTags.useQuery({selectedTags}, { enabled: selectedTags.length > 0 })
   return (
     <>
       <main className="flex flex-col flex-wrap flex-none items-start content-center justify-start min-h-screen bg-gradient-to-b from-slate-600 to-red-900">
@@ -17,10 +18,15 @@ const Test: NextPage = () => {
           <SearchBar selectedTags={selectedTags} selectTag={selectTag} />
         </div>
         <div>
+          <h2>Selected Tags</h2>
           <TagList tags={selectedTags} handleClick={tag => selectTag(selectedTags.filter(t => t != tag))} />
         </div>
         <div>
-          {!!data && <Carousel memes={data.memes} />}
+          <h2>Available Tags</h2>
+          { !!availableTags && <TagList tags={availableTags.tags} handleClick={tag => selectTag([...selectedTags, tag])} /> }
+        </div>
+        <div>
+          {!!memeResults && <Carousel memes={memeResults.memes} />}
         </div>
       </main>
     </>

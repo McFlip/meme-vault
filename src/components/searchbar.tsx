8b85b0fa5177a172bdb3bcb8f09494fe0c8a1564
McFlip <gradydenton@yahoo.com>
2023-04-15 00:18:06 -0400

    clear searchbar after clicking result

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -2,32 +2,42 @@
 import { Input } from "~/components/ui/input"
 import { Label } from "~/components/ui/label"
 import { Dispatch, SetStateAction, useState } from "react"
-import { api } from "~/utils/api";
-import { Search } from "lucide-react";
+import { api } from "~/utils/api"
+import { Search } from "lucide-react"
 
-export function SearchBar({ selectedTags, selectTag }: { selectedTags: string[], selectTag: Dispatch<SetStateAction<string[]>> }) {
-  const [qstr, setQstr] = useState('')
-  const { data, isLoading, isError } = api.tags.getSearchResults.useQuery({ qstr }, { enabled: !!qstr })
+export function SearchBar({
+  selectedTags,
+  selectTag,
+}: {
+  selectedTags: string[]
+  selectTag: Dispatch<SetStateAction<string[]>>
+}) {
+  const [qstr, setQstr] = useState("")
+  const { data, isLoading, isError } = api.tags.getSearchResults.useQuery(
+    { qstr },
+    { enabled: !!qstr }
+  )
 
-  const handleSearchInputChange = (value: HTMLInputElement['value']) => {
+  const handleSearchInputChange = (value: HTMLInputElement["value"]) => {
     setQstr(value)
   }
 
   const listTags = (tags: string[]) => {
     const tagListItems = tags.map((tag, i) => {
       return (
-        <li key={i} className="bg-green-300 shadow-xl rounded-xl p-1 mb-1">
-          <button onClick={_e => selectTag([...selectedTags, tag])}>
+        <li key={i} className="mb-1 rounded-xl bg-green-300 p-1 shadow-xl">
+          <button
+            onClick={(_e) => {
+              selectTag([...selectedTags, tag])
+              setQstr("")
+            }}
+          >
             {tag}
           </button>
         </li>
       )
     })
-    return (
-      <ul className="bg-slate-400">
-        {tagListItems}
-      </ul>
-    )
+    return <ul className="bg-slate-400">{tagListItems}</ul>
   }
 
   return (
@@ -36,13 +46,18 @@ export function SearchBar({ selectedTags, selectTag }: { selectedTags: string[],
         <Label htmlFor="searchbar">Search for tags</Label>
         <span className="inline-flex">
           <Search className="mr-1" />
-          <Input type="search" id="searchbar" value={qstr} onChange={e => handleSearchInputChange(e.target.value)} />
+          <Input
+            type="search"
+            id="searchbar"
+            value={qstr}
+            onChange={(e) => handleSearchInputChange(e.target.value)}
+          />
         </span>
       </div>
       <div className="absolute top-[60px]">
-        {isError && (<p>Error getting tags</p>)}
-        {isLoading && qstr != '' && (<p>Searching...</p>)}
-        {!!data && listTags(data.tags.filter(t => !selectedTags.includes(t)))}
+        {isError && <p>Error getting tags</p>}
+        {isLoading && qstr != "" && <p>Searching...</p>}
+        {!!data && listTags(data.tags.filter((t) => !selectedTags.includes(t)))}
       </div>
     </div>
   )
